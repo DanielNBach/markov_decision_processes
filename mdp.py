d636678cd34decc5286d4c0d8b669586152dc3a9
Daniel Bach <daniel.b7575@gmail.com>
2023-11-23 10:48:42 -0800

    add blackjack P

--- mdp.py
+++ mdp.py
@@ -4,10 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import gymnasium as gym
 import json
+import pickle
 from algorithms.rl import RL
 from algorithms.planner import Planner
+from examples.blackjack import Blackjack
 from collections import defaultdict
 
+np.random.seed(8588)
+
+"""
+MDP 1: Blackjack
+"""
+def blackjack_value_iteration():
+    blackjack = Blackjack()
 
 if __name__ == "__main__":
-    print("Hello")
+    blackjack_value_iteration()
